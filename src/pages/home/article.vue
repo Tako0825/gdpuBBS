507050eb3e56672a5f9afa60fef4025baeb15165
Tako <2060364922@qq.com>
2023-09-25 21:01:19 +0800

    todo-vuex状态管理-分类&登录

--- src/pages/home/article.vue
+++ src/pages/home/article.vue
@@ -1,15 +1,34 @@
 <template>
   <view class="container">
 
-    <!-- 展示: content | createtime | like | likeNumber | star | stars -->
-    <!-- 查询: authorId -> 作者头像和用户名 | articleCategory -> 当前文章所属分类 -->
+    <!-- 用户头像、用户名 -->
+    <view class="author">
+        <view class="author-nickName">{{ authorName }}</view>
+        <view class="author-avatar">
+            <img :src="avatar" alt="用户头像">
+        </view>
+    </view>
+    <!-- 文章详情 -->
     <view class="article">
-        <view class="content">{{ content }}</view>
         <view class="createtime">{{ createtime }}</view>
+        <view class="content">{{ content }}</view>
         <view class="like">{{ like }}</view>
         <view class="likeNumber">{{ likeNumber }}</view>
         <view class="star">{{ star }}</view>
-        <view class="stars">{{ stars }}</view>
+        <view class="stars">{{ stars?stars:0 }}</view>
+    </view>
+
+    <!-- 评论 -->
+    <!-- author | authorPicture | content | createtime -->
+    <view class="commentList" v-for="(item, index) in commentList" :key="index">
+        <view class="user">
+            <view class="user-avatar">
+                <img :src="item.authorPicture" alt="">
+            </view>
+            <view class="user-nickName">{{ item.author }}</view>
+        </view>
+        <view class="comment-content">{{ item.content }}</view>
+        <view class="comment-createtime">{{ item.createtime }}</view>
     </view>
   </view>
 </template>
@@ -20,22 +39,29 @@ export default {
     data() {
         return {
             userId: 1,
+            // 文章详情
             content: undefined,
             createtime: undefined,
             like: undefined,
             likeNumber: undefined,
             star: undefined,
             stars: undefined,
-
+            // 作者详情
+            authorId: undefined,
+            articleCategory: undefined,
+            avatar: undefined,
+            authorName: undefined,
             category: undefined,
-            avator: undefined,
-            authorName: undefined
+            // 评论
+            commentList: new Array()
         }
     },
 
     async onLoad(params) {
         const { id: articleId } = params
         await this.getArticle(articleId)
+        await this.getAuthor()
+        await this.getComment(articleId)
     },
 
     methods: {
@@ -44,13 +70,42 @@ export default {
             const res = await http(`/articles/?articleId=${articleId}&userId=${this.userId}`, {
                 method: "GET"
             })
-            this.article = res.data
-            console.log(this.article);
+            const { content, createtime, like, likeNumber, star, stars, authorId, articleCategory } = res.data.data
+            this.content = content
+            this.createtime = createtime
+            this.like = like
+            this.likeNumber = likeNumber
+            this.star = star
+            this.stars = stars
+            this.authorId = authorId
+            this.articleCategory = articleCategory
+        },
+
+        // 获取作者详情 (头像、用户名)
+        async getAuthor() {
+            const { data: user } = await http(`/users/${this.authorId}`, {
+                method: "GET"
+            })
+            const { picture = "../../static/avatar.png", nickName } = user
+            this.avatar = picture?picture:"../../static/avatar.png"
+            this.authorName = nickName
+        },
+
+        // 获取评论详情
+        async getComment(articleId) {
+            const { data: commentList } = await http(`/comments/${articleId}`, {
+                methods: "GET"
+            })
+            this.commentList = commentList
         }
     }
 }
 </script>
 
 <style>
-
+    .article {
+        display: flex;
+        flex-direction: column;
+        row-gap: 40rpx;
+    }
 </style>
