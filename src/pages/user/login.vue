a2f22bec94fa365fce00343b381d9e7591773fc9
Tako0825 <2060364922@qq.com>
2023-09-27 20:00:17 +0800

    todo-message消息提示

--- src/pages/user/login.vue
+++ src/pages/user/login.vue
@@ -1,8 +1,11 @@
 <template>
-  <view>
-    <input type="text" v-model="username">
-    <input type="text" v-model="password">
-    <button type="button" @click="login">Login</button>
+  <view class="white-background">
+    <view class="container">
+      <view class="gradient-text">Welcome to GDPU BBS!</view>
+      <input name="username" type="text" v-model="username">
+      <input name="password" type="text" v-model="password">
+      <button class="login cu-btn" type="button" @click="login">登 录</button>
+    </view>
   </view>
 </template>
 
@@ -47,12 +50,75 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+.white-background {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #fff;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 50rpx;
+  transform: translateY(-35%);
+  width: 100vw;
+  height: 200vh;
+  
   input {
+    position: relative;
+    width: 600rpx;
+    height: 120rpx;
+    border: 2px solid #ccc;
+    font-size: 20px;
+    border-radius: 10rpx;
+    padding: 0 20rpx;
+  }
+
+  input:hover {
+    border: 2px solid #000;
+    &::before {
+      color: #3498db!important;
+      font-weight: bold;
+    }
+  }
+
+  input[name="username"]::before {
+    position: absolute;
+    top: 0;
+    font-size: 12px;
+    color: #ccc;
+    content: "用户名"
+  }
+
+  input[name="password"]::before {
+    position: absolute;
+    top: 0;
+    font-size: 10px;
+    color: #ccc;
+    content: "密码"
+  }
+
+  .login {
     width: 600rpx;
     height: 100rpx;
-    border: 1px solid #000;
-    border-radius: 20rpx;
-    margin: 20rpx auto;
+    background: linear-gradient(to right, #3498db, #18b3bd);
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
+    border-radius: 50rpx;
   }
+
+  .gradient-text {
+    font-size: 24px;
+    font-weight: bold;
+    background: linear-gradient(to right, #3498db, #18b3bd);
+    -webkit-background-clip: text; /* 使用 WebKit 前缀以支持某些浏览器 */
+    background-clip: text;
+    color: transparent; /* 文本变为透明，以显示背景渐变 */
+  }
+}
 </style>
